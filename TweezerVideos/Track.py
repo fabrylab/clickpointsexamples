ec573337e91ed0516c2b24cac932bb63368c93ec
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2015-11-05 14:58:55 +0100

    * tracking code now uses clickpoints package

--- TweezerVideos/Track.py
+++ TweezerVideos/Track.py
@@ -1,28 +1,22 @@
 from __future__ import print_function, division
-import os
 import sys
 import numpy as np
 from scipy.misc import imread
 import cv2
 
-from MarkerLoad import LoadLog, SaveLog
+from clickpoints.SendCommands import JumpFrames, GetImageName, GetMarkerName
+from clickpoints.MarkerLoad import LoadLog, SaveLog
 
-lk_params = dict(winSize=(8, 8),
-                 maxLevel=0,
-                 criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-print(sys.argv)
-folder = os.path.dirname(sys.argv[1])
+lk_params = dict(winSize=(8, 8), maxLevel=0, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
 start_frame = int(sys.argv[2])
 
-pos = "frame%04d_pos.txt"
-img = "frame%04d.jpg"
 for frame in range(start_frame, start_frame+800):
-    print(os.path.join(folder, pos % frame))
-    points, types = LoadLog(os.path.join(folder, pos % frame))
+    points, types = LoadLog(GetMarkerName(frame))
     p0 = np.array([[point["x"], point["y"]] for point in points]).astype(np.float32)
     try:
-        old_gray = imread(os.path.join(folder, img % frame))
-        frame_gray = imread(os.path.join(folder, img % (frame+1)))
+        old_gray = imread(GetImageName(frame))
+        frame_gray = imread(GetImageName(frame+1))
     except IOError:
         print("Finished")
         break
@@ -32,5 +26,5 @@ for frame in range(start_frame, start_frame+800):
     for i in range(p1.shape[0]):
         point2[i]["x"] = p1[i, 0]
         point2[i]["y"] = p1[i, 1]
-    SaveLog(os.path.join(folder, pos % (frame+1)), point2, types)
-
+    SaveLog(GetMarkerName(frame+1), point2, types)
+    JumpFrames(1)
