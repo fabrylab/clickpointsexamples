873d3cb2db64b8f781961bf46c06b44ef5fd6f7c
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2016-01-15 14:25:13 +0100

    * Track: changed to use database system

--- TweezerVideos/Track.py
+++ TweezerVideos/Track.py
@@ -3,9 +3,11 @@ import sys
 import numpy as np
 from imageio import imread
 import cv2
+import os
+import time
 
-from clickpoints.SendCommands import JumpToFrame, GetImageName, GetMarkerName, HasTerminateSignal, CatchTerminateSignal
-from clickpoints.MarkerLoad import LoadLogIDindexed, SaveLog
+from clickpoints.SendCommands import HasTerminateSignal, CatchTerminateSignal, GetImage, JumpToFrameWait
+from clickpoints.MarkerLoad import GetMarker, SetMarker
 
 CatchTerminateSignal()
 
@@ -13,41 +15,35 @@ lk_params = dict(winSize=(8, 8), maxLevel=0, criteria=(cv2.TERM_CRITERIA_EPS | c
 
 start_frame = int(sys.argv[2])
 
-points, types = LoadLogIDindexed(GetMarkerName(start_frame))
-image_current = imread(GetImageName(start_frame))
-tracking_ids = [id for id in points if not points[id]["processed"]]
+image_current, last_image_id, last_image_frame = GetImage(start_frame)
+points = GetMarker(image=last_image_id, image_frame=last_image_frame, processed=0)
+p0 = np.array([[point.x, point.y] for point in points]).astype(np.float32)
+tracking_ids = [point.track_id for point in points]
+types = [point.type_id for point in points]
+
 if len(tracking_ids) == 0:
     print("Nothing to track")
     sys.exit(-1)
 
-for frame in range(start_frame, start_frame+800):
-    next_image = GetImageName(frame+1)
-    if next_image == "":
-        print("Finished")
+frame = start_frame
+while True:
+    image_next, image_id, image_frame = GetImage(frame+1)
+    if image_next is None:
         break
-    image_next = imread(GetImageName(frame+1))
-    p0 = np.array([[points[id]["x"], points[id]["y"]] for id in tracking_ids]).astype(np.float32)
-    print("Tracking frame number", frame)
-    p1, st, err = cv2.calcOpticalFlowPyrLK(image_current, image_next, p0, None, **lk_params)
-    points2 = {}
-    for index, id in enumerate(tracking_ids):
-        points2[id] = points[id].copy()
-        points2[id]["x"] = p1[index, 0]
-        points2[id]["y"] = p1[index, 1]
-        points2[id]["processed"] = 0
-        points[id]["processed"] = 1
-    try:
-        points_new, types = LoadLogIDindexed(GetMarkerName(frame+1))
-    except:
-        points_new = {}
-    points_new.update(points2)
-    SaveLog(GetMarkerName(frame+1), points_new, types)
-    SaveLog(GetMarkerName(frame), points, types)
-    JumpToFrame(frame+1)
 
-    points = points_new
+    print("Tracking frame number", frame, ",", len(tracking_ids), "tracks")
+    p1, st, err = cv2.calcOpticalFlowPyrLK(image_current, image_next, p0, None, **lk_params)
+
+    SetMarker(image=image_id, image_frame=image_frame, x=p1[:, 0], y=p1[:, 1], processed=0, type=types, track=tracking_ids)
+    JumpToFrameWait(frame+1)
+    SetMarker(image=last_image_id, image_frame=last_image_frame, processed=1, type=types, track=tracking_ids)
+
+    p0 = p1
+    last_image_id = image_id
+    last_image_frame = image_frame
     image_current = image_next
+    frame += 1
+
     if HasTerminateSignal():
         print("Terminating Tracking")
         sys.exit(0)
-
