e1cfdcbb2e0966fe54a8fce99e9b3a4fdc2fa2e9
Fox <57496278+foxsr@users.noreply.github.com>
2016-02-03 09:32:27 +0100

    * Evaluation: updated to new MarkerLoad.py class

--- TweezerVideos/Evaluation.py
+++ TweezerVideos/Evaluation.py
@@ -2,9 +2,12 @@ from __future__ import print_function, division
 import numpy as np
 import matplotlib.pyplot as plt
 
-from clickpoints.MarkerLoad import GetTracks
+from clickpoints.MarkerLoad import *
 
-tracks = GetTracks()
+# open DB File
+db = DataFile("clickpoints.db")
+
+tracks = db.GetTracks()
 for track in tracks:
     points = track.points()
     plt.plot(np.linalg.norm(points[:, :] - points[0, :], axis=1), "-o")
